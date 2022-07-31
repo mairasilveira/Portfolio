c36214ec1e4e3075f00dc459c8613ab3f7cdd4fb
Maira Silveira <silveirasmaira@gmail.com>
2022-07-31 12:52:44 -0300

    alteração nos pássaros do layout

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 let bird1 = document.getElementById('bird1');
-let bird2 = document.getElementById('bird2');
 
 window.addEventListener('scroll', function() {
     let value = window.scrollY;
-    bird2.style.top = value * -1.5 + 'px';
-    bird2.style.left = value * 2 + 'px';
     bird1.style.top = value * -1.5 + 'px';
     bird1.style.left = value * -5 + 'px';
 })
