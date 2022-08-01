6d240efcb74fab05601587d8f75419ad64d265c4
Maira Silveira <silveirasmaira@gmail.com>
2022-08-01 20:56:45 -0300

    alteração da div do título

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-let bird1 = document.getElementById('bird1');
+let bird1 = document.getElementById('butterfly');
 
 window.addEventListener('scroll', function() {
     let value = window.scrollY;
-    bird1.style.top = value * -1.5 + 'px';
-    bird1.style.left = value * -5 + 'px';
+    butterfly.style.top = value * -1.5 + 'px';
+    butterfly.style.left = value * -5 + 'px';
 })
